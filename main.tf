af031164534fe37ac213c8f2f9a67fa34f93c90a
mohamed-hashicorp <mohamed.abdelbaset@hashicorp.com>
2026-02-25 15:01:25 +0100

    update

--- main.tf
+++ main.tf
@@ -1,29 +1,24 @@
-#provider "aws" {
-#  region = "us-east-2"
-#}
-
-#resource "aws_instance" "example" {
-#  ami           = "ami-0c55b159cbfafe1f0"
-#  instance_type = "t2.micro"
-#  vpc_security_group_ids = [aws_security_group.instance.id]
-#  user_data = <<-EOF
-#              #!/bin/bash
-#             echo "Hello, World" > index.html
-#             nohup busybox httpd -f -p 8080 &
-#             EOF
-
-resource "null_resource" "this" {
+provider "aws" {
+  region = "us-east-2"
 }
 
-resource "null_resource" "this1" {
-}
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+             echo "Hello, World" > index.html
+             nohup busybox httpd -f -p 8080 &
+             EOF
 
-resource "null_resource" "this2" {
+
+
+
+ tags = {
+   Name = "terraform-example"
+ }
 }
-# tags = {
-#   Name = "terraform-example"
-# }
-#}
 
 #resource "aws_security_group" "instance" {
 #  name = "terraform-example-instance"
